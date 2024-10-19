1d893867c8aba7592ecadd29d018aaccae356deb
Nirmal-wishwantha <nirmalwishwantha@gmail.com>
2024-10-19 22:28:48 +0530

    some problems fixed.

--- src/main/java/lk/riyapola/system/repo/ReservationRepo.java
+++ src/main/java/lk/riyapola/system/repo/ReservationRepo.java
@@ -2,12 +2,19 @@ package lk.riyapola.system.repo;
 
 import lk.riyapola.system.entity.Reservation;
 import lk.riyapola.system.entity.Vehicle;
+import lk.riyapola.system.status.ReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 
 
 public interface ReservationRepo extends JpaRepository<Reservation, Integer> {
 
-Reservation findById(int id);
+    Reservation findById(int id);
+
+
+    List<Reservation> findByStatus(ReservationStatus status);
+    public abstract Reservation findByVehicleAndReservationDate(Vehicle vehicle, LocalDate reservationDate);
 
 }
